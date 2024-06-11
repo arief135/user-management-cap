74dd7a2c6c817e84e16e563c48bd59c4c8e58c0b
Arief Pratama <ariefpratama135@gmail.com>
2024-06-11 21:11:58 +0700

    update custom basic auth

--- srv/basic-auth.ts
+++ srv/basic-auth.ts
@@ -3,30 +3,33 @@ import { Request, Response, NextFunction } from "express";
 
 type Req = Request & { user: cds.User, tenant: string };
 
+const { Users } = cds.entities('id.apnv.usermanagement')
+
+const findUser = async (user: string) => {
+    return await SELECT.one.from(Users, user)
+}
+
 export default function basicAuth(req: Req, res: Response, next: NextFunction) {
     // do your custom authentication ...
 
     const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
     const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
 
-    let userValid = false
+    return findUser(login).then((user: string) => {
+        if (user) {
+            req.user = new cds.User({
+                id: user,
+                roles: ['ADMINISTRATOR', 'DEVELOPER'],
+                attr: {
+                    attra: '<value>',
+                    attrb: '<value>'
+                }
+            })
+            return next()
 
-    if (login == 'arief') {
-        userValid = true
-        req.user = new cds.User({
-            id: 'arief',
-            roles: ['ADMINISTRATOR', 'DEVELOPER'],
-            attr: {
-                attra: '<value>',
-                attrb: '<value>'
-            }
-        })
-    }
-
-    if (userValid) {
-        return next()
-    } else {
-        res.set('WWW-Authenticate', 'Basic realm="401"')
-        res.status(401).send('Authentication required.')
-    }
+        } else {
+            res.set('WWW-Authenticate', 'Basic realm="401"')
+            res.status(401).send('Authentication required.')
+        }
+    })
 }
